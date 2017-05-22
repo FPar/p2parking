77efc3ee23986a377abd582fca0b41a4c1d79980
Fabian Parzefall <fabian.parzefall@mailbox.org>
2017-05-22 10:39:09 +0200

    Initial commit

---
+++ src/application/P2P.h
@@ -0,0 +1,23 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef APPLICATION_P2P_H_
+#define APPLICATION_P2P_H_
+
+class P2P: public BaseWaveApplLayer {
+public:
+};
+
+#endif /* APPLICATION_P2P_H_ */
